3b02622d144b0eb44c9f498f692042ffeeb1dc0e
kadjialan <alankadji7@gmail.com>
2023-06-18 19:28:51 +0100

    linters resolution

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,30 +1,30 @@
-const form = document.querySelector("form");
-const userContainer = document.querySelector(".all-users");
+const form = document.querySelector('form')
+const userContainer = document.querySelector('.all-users')
 
-function displayUser({ age, name }) {
+function displayUser ({ age, name }) {
   return `<div class="user">
     <div class="details">
         <h2>${name}</h2>
-        <p>${age} years${age > 1 ? "s" : ""} old</p>
+        <p>${age} years${age > 1 ? 's' : ''} old</p>
     </div>
-</div>`;
+</div>`
 }
 
-function displayUsers(persons) {
+function displayUsers (persons) {
   return persons.length
-    ? persons.map(displayUser).join("")
-    : renderMessage("Sorry! No user found");
+    ? persons.map(displayUser).join('')
+    : renderMessage('Sorry! No user found')
 }
 
-function compareNames(name, searchTerm) {
-  return name.toLowerCase().includes(searchTerm.toLowerCase());
+function compareNames (name, searchTerm) {
+  return name.toLowerCase().includes(searchTerm.toLowerCase())
 }
 
-function shouldResolve() {
-  return Math.random() < 0.85;
+function shouldResolve () {
+  return Math.random() < 0.85
 }
 
-function searchUsers(name, age) {
+function searchUsers (name, age) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve()) {
@@ -34,30 +34,30 @@ function searchUsers(name, age) {
               (!name || compareNames(user.name, name)) &&
               (!age || user.age === age)
           )
-        );
+        )
       } else {
-        reject([]);
+        reject([])
       }
-    }, 3000);
-  });
+    }, 3000)
+  })
 }
 
-function renderMessage(message) {
-  return `<div class="message">${message}</div>`;
+function renderMessage (message) {
+  return `<div class="message">${message}</div>`
 }
 
-userContainer.innerHTML = displayUsers(users);
+userContainer.innerHTML = displayUsers(users)
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  userContainer.innerHTML = renderMessage("Searching users....");
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  userContainer.innerHTML = renderMessage('Searching users....')
   searchUsers(e.target.name.value, +e.target.age.value)
     .then((result) => {
-      userContainer.innerHTML = displayUsers(result);
+      userContainer.innerHTML = displayUsers(result)
     })
     .catch((e) => {
       userContainer.innerHTML = renderMessage(
-        "Error loading users! please try again"
-      );
-    });
-});
+        'Error loading users! please try again'
+      )
+    })
+})
