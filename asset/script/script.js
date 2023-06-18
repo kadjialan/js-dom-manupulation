577f62b0fe376cdec846f7e054a0f9a3b607513d
kadjialan <alankadji7@gmail.com>
2023-06-18 19:17:56 +0100

    linters resolution

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,69 +1,63 @@
-const form = document.querySelector('form');
-const userContainer = document.querySelector('.all-users');
+const form = document.querySelector("form");
+const userContainer = document.querySelector(".all-users");
 
 function displayUser({ age, name }) {
-    return `<div class="user">
+  return `<div class="user">
     <div class="details">
         <h2>${name}</h2>
         <p>${age} years${age > 1 ? "s" : ""} old</p>
     </div>
-</div>`
-
+</div>`;
 }
 
 function displayUsers(persons) {
-    return persons.length
-        ? persons.map(displayUser).join("")
-        : renderMessage("Sorry! No user found");
+  return persons.length
+    ? persons.map(displayUser).join("")
+    : renderMessage("Sorry! No user found");
 }
 
 function compareNames(name, searchTerm) {
-    return name.toLowerCase().includes(searchTerm.toLowerCase());
+  return name.toLowerCase().includes(searchTerm.toLowerCase());
 }
 
 function shouldResolve() {
-    return Math.random() < 0.85;
+  return Math.random() < 0.85;
 }
 
 function searchUsers(name, age) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (shouldResolve()) {
-                resolve(
-                    users.filter(
-                        (user) =>
-                            (!name || compareNames(user.name, name)) &&
-                            (!age || user.age === age)
-                    )
-                );
-
-            } else {
-                reject([]);
-            }
-
-        },3000);
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve()) {
+        resolve(
+          users.filter(
+            (user) =>
+              (!name || compareNames(user.name, name)) &&
+              (!age || user.age === age)
+          )
+        );
+      } else {
+        reject([]);
+      }
+    }, 3000);
+  });
 }
 
 function renderMessage(message) {
-    return `<div class="message">${message}</div>`
+  return `<div class="message">${message}</div>`;
 }
 
 userContainer.innerHTML = displayUsers(users);
 
-
-
-
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    userContainer.innerHTML = renderMessage("Searching users....");
-    searchUsers(e.target.name.value, +e.target.age.value)
-        .then((result) => {
-            userContainer.innerHTML = displayUsers((result));
-        })
-        .catch((e) => {
-            userContainer.innerHTML = renderMessage(
-                "Error loading users! please try again"
-            );
-        });
+  e.preventDefault();
+  userContainer.innerHTML = renderMessage("Searching users....");
+  searchUsers(e.target.name.value, +e.target.age.value)
+    .then((result) => {
+      userContainer.innerHTML = displayUsers(result);
+    })
+    .catch((e) => {
+      userContainer.innerHTML = renderMessage(
+        "Error loading users! please try again"
+      );
+    });
 });
