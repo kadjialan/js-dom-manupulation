124e6683183bf71fbd6313b1ca61e122dcda7dbc
kadjialan <alankadji7@gmail.com>
2023-07-13 11:20:01 +0100

    delete implementation

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,97 +1,136 @@
 const users = [
-  { name: 'Kadji Danel', age: 18 },
-  { name: 'Alan James', age: 41 },
-  { name: 'Gaetan Vianey', age: 30 },
-  { name: 'Charles Evans', age: 55 },
-  { name: 'Bitz Boy', age: 50 },
-  { name: 'Rash Junior', age: 56 },
-  { name: 'Joshua Joshi', age: 47 },
-  { name: 'Emmanual Atangana', age: 48 },
-  { name: 'Alaric Kevin', age: 60 },
-  { name: 'Christ Godden', age: 50 },
-  { name: 'Nathaneal Mesmer', age: 24 },
-  { name: 'Ange Joelle', age: 29 },
-  { name: 'Nehal Gringo', age: 53 },
-  { name: 'Nicole Mekongo', age: 43 },
-  { name: 'Jessica Keyoma', age: 32 },
-  { name: 'John Paul', age: 20 },
-  { name: 'Jones Adam', age: 17 },
-  { name: 'Johnny Alide', age: 34 },
-  { name: 'Pryan Patrick', age: 26 },
-  { name: 'Cathy Ntoulo', age: 59 },
-  { name: 'Paul Bamock', age: 15 }
-]
+  { name: "Kadji Danel", age: 18 },
+  { name: "Alan James", age: 41 },
+  { name: "Gaetan Vianey", age: 30 },
+  { name: "Charles Evans", age: 55 },
+  { name: "Bitz Boy", age: 50 },
+  { name: "Rash Junior", age: 56 },
+  { name: "Joshua Joshi", age: 47 },
+  { name: "Emmanual Atangana", age: 48 },
+  { name: "Alaric Kevin", age: 60 },
+  { name: "Christ Godden", age: 50 },
+  { name: "Nathaneal Mesmer", age: 24 },
+  { name: "Ange Joelle", age: 29 },
+  { name: "Nehal Gringo", age: 53 },
+  { name: "Nicole Mekongo", age: 43 },
+  { name: "Jessica Keyoma", age: 32 },
+  { name: "John Paul", age: 20 },
+  { name: "Jones Adam", age: 17},
+  { name: "Johnny Alide", age: 34},
+  { name: "Pryan Patrick", age: 26},
+  { name: "Cathy Ntoulo", age: 59},
+  { name: "Paul Bamock", age: 15},
+];
 
-const form = document.querySelector('form')
-const userContainer = document.querySelector('.all-users')
+const form = document.querySelector("form");
+const userContainer = document.querySelector(".all-users");
+const acronym = document.querySelector(".acronyms");
+const numb = document.querySelector(".numb");
+const fullname = document.querySelector(".fullname");
 
-function getInitial (name) {
-  return name
-    .split(' ')
-    .map((n) => n[0])
-    .slice(0, 2)
-    .join('.')
+// Function to filter users by name or age
+function filterUsers(name, age) {
+  const filteredUsers = users.filter((user) => {
+    if (age) {
+      return user.age === age;
+    }
+    if (name !== "") {
+      return (
+        user.name.toLowerCase() === name ||
+        user.name.toLowerCase().includes(name)
+      );
+    }
+
+    if (age && name !== "") {
+      return user.age === age && user.name.toLowerCase() === name;
+    }
+  });
+
+  return filteredUsers;
 }
 
-function displayUser ({ age, name }) {
-  return `<div class="user">
-    <div class="details">
-        <h2>${getInitial(name)}</h2>
-        <h3>${name}</h3>
-        <p>${age} years${age > 1 ? 's' : ''} old</p>
-        <button>&cross;</button>
+// Function to display filtered users
+function displayUsers(filteredUsers) {
+  console.log(filteredUsers);
+  const resultsContainer = document.getElementById("all-users");
+  resultsContainer.innerHTML = "";
+
+  if (filteredUsers.length === 0) {
+    resultsContainer.innerHTML = "<p class='error'>No user found.</p>";
+    return;
+  }
+
+  function getInitial(name) {
+    return name
+      .split(" ")
+      .map((n) => n[0])
+      .slice(0, 2)
+      .join(".");
+  }
+
+  filteredUsers.forEach((user) => {
+    const userDiv = document.createElement("div");
+    userDiv.classList.add("user");
+
+    userDiv.innerHTML = ` 
+    <div class="details"> 
+    <h2 class="acronyms">${getInitial(user.name)}</h2>
+    <h3 class="fullname">Name: ${user.name}</h3>
+       <p class="numb">Age: ${user.age}</p>
+    <button class="delete-btn">&cross;</button>
     </div>
-  
-</div>`
+`;
+
+    const deleteBtn = userDiv.querySelector(".delete-btn");
+    deleteBtn.addEventListener("click", () => {
+      deleteUser(user);
+    });
+
+    resultsContainer.appendChild(userDiv);
+  });
 }
 
-function displayUsers (persons) {
-  return persons.length
-    ? persons.map(displayUser).join('')
-    : renderMessage('Sorry! No user found')
+displayUsers(users);
+
+// Function to delete a user
+function deleteUser(user) {
+  console.log(user.name);
+  const newArray = users.filter((element) => user.name !== element.name);
+  console.log(newArray);
+  displayUsers(newArray);
 }
 
-function compareNames (name, searchTerm) {
-  return name.toLowerCase().includes(searchTerm.toLowerCase())
+// Function to handle search button click
+function handleSearch(nameInput , ageInput ) {
+  const name = nameInput.trim().toLowerCase();
+  const age = ageInput;
+  console.log(nameInput.value, age.valueOf());
+
+  const filteredUsers = filterUsers(name, age);
+  displayUsers(filteredUsers);
 }
 
-function shouldResolve () {
-  return Math.random() < 0.85
+// Function to show loader while searching users
+function showLoader() {
+  const loader = document.getElementById("loader");
+  loader.classList.remove("hidden");
 }
 
-function searchUsers (name, age) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (shouldResolve()) {
-        resolve(
-          users.filter(
-            (user) =>
-              (!name || compareNames(user.name, name)) &&
-              (!age || user.age === age)
-          )
-        )
-      } else {
-        reject(new Error('something bad happened'))
-      }
-    }, 3000)
-  })
+// Function to hide loader
+function hideLoader() {
+  const loader = document.getElementById("loader");
+  loader.classList.add("hidden");
 }
 
-function renderMessage (message) {
-  return `<div class="message">${message}</div>`
-}
-userContainer.innerHTML = displayUsers(users)
+// Event listener for  form
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  userContainer.innerHTML = renderMessage('Searching users....')
-  searchUsers(e.target.name.value, +e.target.age.value)
-    .then((result) => {
-      userContainer.innerHTML = displayUsers(result)
-    })
-    .catch((e) => {
-      userContainer.innerHTML = renderMessage(
-        'Error loading users! please try again'
-      )
-    })
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  showLoader();
+
+  setTimeout(() => {
+    handleSearch(e.target.name.value, +e.target.age.value);
+    hideLoader();
+  }, 1000);
+}); 
+
